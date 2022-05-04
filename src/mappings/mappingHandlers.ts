8eb7a535dd13de82a28867c13f5c0b1929ce6611
tane-git <tane.alexander.wilson@gmail.com>
2022-05-05 11:11:45 +1200

    update specVersion

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -11,23 +11,23 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
     await record.save();
 }
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
-}
+// export async function handleEvent(event: SubstrateEvent): Promise<void> {
+//     const {event: {data: [account, balance]}} = event;
+//     //Retrieve the record by its ID
+//     const record = await StarterEntity.get(event.block.block.header.hash.toString());
+//     record.field2 = account.toString();
+//     //Big integer type Balance of a transfer event
+//     record.field3 = (balance as Balance).toBigInt();
+//     await record.save();
+// }
 
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
-}
+// export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
+//     const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
+//     //Date type timestamp
+//     record.field4 = extrinsic.block.timestamp;
+//     //Boolean tyep
+//     record.field5 = true;
+//     await record.save();
+// }
 
 
